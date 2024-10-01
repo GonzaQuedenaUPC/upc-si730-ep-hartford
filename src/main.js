0f1cf4d925b786a2a7ac586629e4b1de1b3a9bde
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-10-01 11:41:55 -0500

    chore: add primevue components to main.js.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,31 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+import 'primeflex/primeflex.css';
+
+//PrimeVue components
+import Toolbar from "primevue/toolbar";
+import Card from "primevue/card";
+import SelectButton from "primevue/selectbutton";
+
+const app = createApp(App);
+
+app.use(PrimeVue, {
+    ripple: true, theme: {
+        preset: Aura, options: {
+            prefix: 'p',
+            darkModeSelector: 'system',
+            cssLayer: false
+        }
+    }
+});
+
+app.component('pv-toolbar', Toolbar)
+    .component('pv-card', Card)
+    .component('pv-select-button', SelectButton);
+
+app.mount('#app');
