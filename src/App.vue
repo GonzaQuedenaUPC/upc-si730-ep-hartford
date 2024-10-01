f34df47ac5483075d7f7c656f61ca008035ae415
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-10-01 12:12:52 -0500

    feat(public): add toolbar component.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
-<script setup>
+<script>
+import Toolbar from './public/components/toolbar.component.vue'
+
+export default {
+  name: 'App',
+
+  components: {
+    Toolbar
+  }
+}
 
 </script>
 
 <template>
-
+  <nav class="nav">
+    <toolbar/>
+  </nav>
 </template>
 
 <style scoped>
---
+++ src/public/components/toolbar.component.vue
@@ -0,0 +1,84 @@
+<script>
+export default {
+  name: "toolbar"
+}
+</script>
+
+<template>
+  <pv-toolbar class="navbar__container">
+    <template #start>
+      <div class="navbar__logo">
+        <img src="https://hign.org/sites/default/files/2022-01/HIGN-logo-1200.png" alt="hign-logo" />
+      </div>
+    </template>
+
+    <template #center>
+      <span class="navbar__title">
+        Hartford Institute for Geriatric Nursing
+      </span>
+    </template>
+
+    <template #end>
+      <ul class="navbar__list">
+        <li class="navbar__item">
+          <router-link to="">
+            <pv-button>
+              Home
+            </pv-button>
+          </router-link>
+        </li>
+
+        <li class="navbar__item">
+          <router-link to="">
+            <pv-button>
+              Examiner Performance Overview
+            </pv-button>
+          </router-link>
+        </li>
+      </ul>
+    </template>
+  </pv-toolbar>
+</template>
+
+<style scoped>
+
+.navbar__container {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  padding: 20px;
+}
+
+.navbar__logo img {
+  width: 90%;
+  height: 50px;
+}
+
+.navbar__title {
+  text-align: center;
+}
+
+.navbar__list {
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  list-style: none;
+  gap: 20px;
+}
+
+@media screen and (min-width: 768px) {
+  .navbar__container {
+    flex-direction: row;
+    align-items: center;
+  }
+
+  .navbar__title {
+    text-align: center;
+  }
+
+  .navbar__list {
+    flex-direction: row;
+  }
+}
+
+</style>
