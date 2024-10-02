07f985917fa4d4a26e3a4db6c2d0c4168386bed2
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-10-02 02:40:38 -0500

    feat(nursing): add examiner card implementation in examiner performance overview.

---
+++ src/nursing/components/examiner-card.component.vue
@@ -0,0 +1,58 @@
+<script>
+import Examiner from "../models/examiner.entity.js";
+
+export default {
+  name: "examiner-card",
+
+  props: {
+    examiner: {
+      type: Examiner,
+      required: true
+    },
+    assignedExamCount: {
+      type: Number,
+      required: true,
+      default: 0
+    },
+    averageExamScore: {
+      type: Number,
+      required: true,
+      default: 0
+    }
+  },
+
+  methods: {
+    getFullName() {
+      return `${this.examiner.firstName} ${this.examiner.lastName}`;
+    }
+  }
+
+}
+</script>
+
+<template>
+  <pv-card class="card" style="width: 30%; margin: 1rem;">
+    <template #header>
+      <h2 class="card__header">{{ getFullName() }}</h2>
+    </template>
+
+    <template #title>
+      <h4 class="card__title">NPI: {{ examiner.nationalProviderIdentifier }}</h4>
+    </template>
+
+    <template #subtitle>
+      <p class="card__subtitle">Mental State Exam Performance</p>
+    </template>
+
+    <template #content>
+      <div class="card__info">
+        <p><strong>Current Assigned Mental State Exam Count: </strong> {{ assignedExamCount }}</p>
+        <p><strong>Average Assigned Mental State Exam Total Score: </strong> {{ averageExamScore }}</p>
+      </div>
+    </template>
+  </pv-card>
+</template>
+
+<style scoped>
+
+</style>
--- src/nursing/pages/examiner-performance-overview.component.vue
+++ src/nursing/pages/examiner-performance-overview.component.vue
@@ -1,14 +1,141 @@
 <script>
+import ExaminerService from "../services/examiner.service.js";
+import MentalStateExamService from "../services/mental-state-exam.service.js";
+
+import ExaminerCard from '../components/examiner-card.component.vue';
+import Examiner from "../models/examiner.entity.js";
 
 export default {
-  name: "examiner-performance-overview"
+  name: "examiner-performance-overview",
+
+  components: {
+    ExaminerCard
+  },
+
+  data() {
+    return {
+      examiners: [],
+      examinerApi: new ExaminerService(),
+
+      exams: [],
+      mentalStateExamApi: new MentalStateExamService(),
+    }
+  },
+
+  methods: {
+
+    getAssignedExams(id) {
+      return this.exams.filter(exam => exam.examinerId === id);
+    },
+
+    calculateAssignedExamCount(assignedExams) {
+      return assignedExams.length || 0;
+    },
+
+    calculateAverageExamScore(assignedExams) {
+      const totalScoreSum = assignedExams.reduce(
+          (sum, exam) =>
+              sum +
+              (parseInt(exam.orientationScore) || 0) +
+              (parseInt(exam.registrationScore) || 0) +
+              (parseInt(exam.attentionAndCalculationScore) || 0) +
+              (parseInt(exam.recallScore) || 0) +
+              (parseInt(exam.languageScore) || 0),
+          0
+      );
+      const assignedExamCount = this.calculateAssignedExamCount(assignedExams);
+
+      return assignedExamCount > 0 ? parseFloat((totalScoreSum / assignedExamCount).toFixed(2)) : 0;
+    },
+
+    updateExaminerWithMetrics(examiner, assignedExamCount, averageExamScore) {
+      const updatedExaminer = new Examiner(
+          examiner.id,
+          examiner.firstName,
+          examiner.lastName,
+          examiner.nationalProviderIdentifier
+      );
+      updatedExaminer.assignedExamCount = assignedExamCount;
+      updatedExaminer.averageExamScore = averageExamScore;
+
+      return updatedExaminer;
+    },
+
+    calculateExamMetricsForExaminers() {
+      this.examiners = this.examiners.map(examiner => {
+        const assignedExams = this.getAssignedExams(examiner.id);
+        const assignedExamCount = this.calculateAssignedExamCount(assignedExams);
+        const averageExamScore = this.calculateAverageExamScore(assignedExams);
+
+        return this.updateExaminerWithMetrics(examiner, assignedExamCount, averageExamScore);
+      });
+    },
+
+    buildExaminerFromApiResponse(response) {
+      return response.map(examiner => {
+        return new Examiner (
+            examiner.id,
+            examiner.firstName,
+            examiner.lastName,
+            examiner.nationalProviderIdentifier
+        );
+      });
+    },
+
+    fetchExaminers() {
+      return this.examinerApi.getAll()
+          .then(response => {
+            //building examiner objects from response
+            return this.buildExaminerFromApiResponse(response.data);
+          });
+    },
+
+    fetchExams() {
+      return this.mentalStateExamApi.getAll()
+          .then(response => {
+            return response.data;
+          });
+    },
+
+    loadExaminersAndExamsData() {
+
+      this.fetchExaminers()
+          .then(examiners => {
+            this.examiners = examiners;
+            //loading exams after examiners
+            return this.fetchExams();
+          })
+          .then(exams => {
+            this.exams = exams;
+            //calculating metrics after loading exams
+            this.calculateExamMetricsForExaminers();
+          })
+          .catch(err => {
+            console.error("Error loading data:", err);
+          });
+    },
+  },
+
+  created() {
+    this.loadExaminersAndExamsData();
+  }
 }
 </script>
 
 <template>
-
+  <div class="cards">
+    <examiner-card v-for="examiner in examiners" :key="examiner.id" :examiner="examiner"
+                   :assigned-exam-count="examiner.assignedExamCount" :average-exam-score="examiner.averageExamScore"/>
+  </div>
 </template>
 
 <style scoped>
+
+.cards {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  justify-items: center;
+  gap: 1rem;
+}
 
 </style>
