11288cb7fde88432c4ab4e9e18da96058f2aa4e2
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-10-02 01:17:38 -0500

    feat(public): add implementation of mental state exam card component.

--- src/public/pages/home.component.vue
+++ src/public/pages/home.component.vue
@@ -1,6 +1,12 @@
 <script>
+import MentalStateExamCard from "../../nursing/components/mental-state-exam-card.component.vue";
+
 export default {
-name: "home"
+  name: "home",
+
+  components: {
+    MentalStateExamCard
+  }
 }
 </script>
 
@@ -8,22 +14,18 @@ name: "home"
   <div class="container">
     <div class="content">
       <h1>Welcome to HIGN</h1>
+      <mental-state-exam-card/>
     </div>
   </div>
 </template>
 
 <style scoped>
 
-.container {
-  width: 90%;
-  margin: 0 auto;
-  height: 100vh;
-}
-
 .content {
-  width: 80%;
-  margin: 0 auto;
-  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
 }
 
 </style>
